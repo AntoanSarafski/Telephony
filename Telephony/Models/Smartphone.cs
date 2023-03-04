04fb17f497c9d65785495d95f3a6e24bcecf9799
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-04 23:05:43 +0200

    Added Smartphone and StationaryPhone classes.

---
+++ Telephony/Models/Smartphone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Models.Interaces;
+
+namespace Telephony.Models
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public string Browse(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Call(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Telephony/Models/StationaryPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Models.Interaces;
+
+namespace Telephony.Models
+{
+    public class StationaryPhone : ICallable
+    {
+        public string Call(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
