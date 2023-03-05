83a8e13072a4d71f831947063f12d1035da44e90
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-05 09:49:12 +0200

    Added Core folder for Engine class and IEngine interface for starting the project.

---
+++ Telephony/Core/Engine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telephony.Core.Interfaces;
+using Telephony.Models;
+using Telephony.Models.Interaces;
+
+namespace Telephony.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            string[] phoneNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] urls = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            ICallable callable;
+
+            foreach (var phoneNumber in phoneNumbers)
+            {
+
+
+                if (phoneNumber.Length == 10)
+                {
+                    callable = new Smartphone();
+
+                }
+                else
+                {
+                    callable = new StationaryPhone();
+                }
+
+                try
+                {
+                    Console.WriteLine(callable.Call(phoneNumber));
+                }
+
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            IBrowsable browsable = new Smartphone();
+
+            foreach (var url in urls)
+            {
+
+                try
+                {
+                    Console.WriteLine(browsable.Browse(url));
+                }
+
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
--- Telephony/StartUp.cs
+++ Telephony/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Telephony.Core;
+using Telephony.Core.Interfaces;
 using Telephony.Models;
 using Telephony.Models.Interaces;
 
@@ -8,52 +10,8 @@ namespace Telephony
     {
         static void Main(string[] args)
         {
-            string[] phoneNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-            string[] urls = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-            ICallable callable;
-
-            foreach (var phoneNumber in phoneNumbers)
-            {
-
-
-                if (phoneNumber.Length == 10)
-                {
-                    callable = new Smartphone();
-                    
-                }
-                else
-                {
-                    callable = new StationaryPhone();
-                }
-
-                try
-                {
-                    Console.WriteLine(callable.Call(phoneNumber));
-                }
-
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            IBrowsable browsable = new Smartphone();
-
-            foreach (var url in urls)
-            {
-                
-                try
-                {
-                    Console.WriteLine(browsable.Browse(url));
-                }
-
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
+            IEngine engine = new Engine();
+            engine.Run();
         }
     }
 }
